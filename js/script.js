eb448c035b5d750d9facb23b6e09f8f9ffbbb865
Sandipan Mukherjee <133745376+SandipanIO@users.noreply.github.com>
2023-06-09 00:26:38 +0530

    Added form validation feature

--- js/script.js
+++ js/script.js
@@ -82,6 +82,45 @@ popupOverlay.addEventListener('click', e => {
 });
 
 /*******************************************************************/
+/* Popup form validation */
+/*******************************************************************/
+
+const popupForm = document.querySelector('#popup__form');
+const popupName = document.querySelector('#popup__name');
+const popupEmail = document.querySelector('#popup__email');
+
+const patternName = /^[a-zA-Z ]{6,}$/;
+const patternEmail = /[^\s@]+@[^\s@]+\.[^\s@]+/;
+
+popupForm.addEventListener('submit', e => {
+   e.preventDefault();
+
+   if(popupName.value === '') {
+      popupName.style.border = '1px solid red';
+   }
+
+   if(popupEmail.value === '') {
+      popupEmail.style.border = '1px solid red';
+   }
+});
+
+popupName.addEventListener('keyup', e => {
+   if(patternName.test(popupName.value)) {
+      popupName.style.border = '1px solid green';
+   } else {
+      popupName.style.border = '1px solid red';
+   }
+});
+
+popupEmail.addEventListener('keyup', e => {
+   if(patternEmail.test(popupEmail.value)) {
+      popupEmail.style.border = '1px solid green';
+   } else {
+      popupEmail.style.border = '1px solid red';
+   }
+});
+
+/*******************************************************************/
 /* Sliding Contact Form */
 /*******************************************************************/
 
@@ -103,4 +142,56 @@ closeContact.addEventListener('click', e => {
 contactOverlay.addEventListener('click', e => {
    contact.setAttribute('data-show-form', 'false');
    contactOverlay.classList.remove('overlay--active');
+});
+
+/*******************************************************************/
+/* Popup form validation */
+/*******************************************************************/
+
+const contactForm = document.querySelector('#contact__form');
+const contactName = document.querySelector('#contact__name');
+const contactEmail = document.querySelector('#contact__email');
+const contactMessage = document.querySelector('#contact__message');
+
+const patternContactText = /^[a-zA-Z ]{5,}$/;
+const patternContactEmail = /(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i;
+
+contactForm.addEventListener('submit', e => {
+   e.preventDefault();
+
+   if(contactName.value === '') {
+      contactName.style.border = '1px solid red';
+   }
+
+   if(contactEmail.value === '') {
+      contactEmail.style.border = '1px solid red';
+   }
+
+   if(contactMessage.value === '') {
+      contactMessage.style.border = '1px solid red';
+   }
+});
+
+contactName.addEventListener('keyup', e => {
+   if(patternContactText.test(contactName.value)) {
+      contactName.style.border = '1px solid green';
+   } else {
+      contactName.style.border = '1px solid red';
+   }
+});
+
+contactEmail.addEventListener('keyup', e => {
+   if(patternContactEmail.test(contactEmail.value)) {
+      contactEmail.style.border = '1px solid green';
+   } else {
+      contactEmail.style.border = '1px solid red';
+   }
+});
+
+contactMessage.addEventListener('keyup', e => {
+   if(patternContactText.test(contactMessage.value)) {
+      contactMessage.style.border = '1px solid green';
+   } else {
+      contactMessage.style.border = '1px solid red';
+   }
 });
