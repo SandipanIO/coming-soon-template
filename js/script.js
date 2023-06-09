5b1baff987bb899245b975b976f9474e0bd2d58a
Sandipan Mukherjee <133745376+SandipanIO@users.noreply.github.com>
2023-06-09 12:37:32 +0530

    Fixed a bug

--- js/script.js
+++ js/script.js
@@ -52,6 +52,8 @@ const countdown = () => {
       hoursLeft.innerHTML = `00<span>Hours</span>`;
       minutesLeft.innerHTML = `00<span>Minutes</span>`;
       secondsLeft.innerHTML = `00<span>Seconds</span>`;
+
+      clearInterval(countdown);
    }
 }
 
@@ -154,7 +156,7 @@ const contactEmail = document.querySelector('#contact__email');
 const contactMessage = document.querySelector('#contact__message');
 
 const patternContactText = /^[a-zA-Z ]{5,}$/;
-const patternContactEmail = /(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i;
+const patternContactEmail = /[^\s@]+@[^\s@]+\.[^\s@]+/;
 
 contactForm.addEventListener('submit', e => {
    e.preventDefault();
